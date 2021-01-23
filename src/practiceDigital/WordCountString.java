1ff5d7f70adaaaf17fbb3bc6704baabf273240d7
AMAN VERMA <amanvrm13@gmail.com>
2021-01-23 13:23:57 +0530

    rev1

---
+++ src/practiceDigital/WordCountString.java
@@ -0,0 +1,62 @@
+package practiceDigital;
+
+import java.util.*;
+
+public class WordCountString {
+
+	public static void main(String[] args) {
+		
+		//To check if letter is there in string of a certain length
+//		boolean b = false;
+//		Scanner s = new Scanner(System.in);
+//			String a = s.nextLine();
+//			System.out.println(a.length());
+//			for (int i = 0; i < a.length(); i++) {
+//				if (a.charAt(i) == 'l') {
+//					b = true;
+//					break;
+//				}
+//			}
+//		
+//			System.out.println(b);
+//		s.close();
+
+		//To Check if  word is present in given string 
+		
+//		System.out.println("enter  string");
+//		Scanner n = new Scanner(System.in);
+//		String str = n.nextLine();
+//		boolean check = str.contains("help");
+//		System.out.println("does this contain string mentioned: " + check);
+//		n.close();
+
+		//to check first and last occurence of certain letter in a string
+		
+	Scanner n = new Scanner(System.in);
+	String str=n.nextLine();
+	
+	int len=str.length();
+	System.out.println(len);
+	for(int i=0 ; i<len ;i++)
+	{
+		if (str.charAt(i)=='a')
+			{
+			System.out.println("char first found at"+ i);
+			break;
+			}
+	}
+System.out.println(str.lastIndexOf('a'));
+//	String str1=str;
+//	int len1=str1.length();
+//	for(int j=len1;j>=0;j--)
+//	{
+//		if (str1.charAt(j)=='a')
+//			{
+//			System.out.println("char last found at"+ j);
+//			break;
+//			}
+//	}
+//		
+		
+	}
+}
---
+++ src/practiceDigital/stringIteration.java
@@ -0,0 +1,10 @@
+package practiceDigital;
+
+public class stringIteration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
