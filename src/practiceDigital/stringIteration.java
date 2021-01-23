6838a77ef717537c1756f999ca15aa72dbb24782
AMAN VERMA <amanvrm13@gmail.com>
2021-01-23 13:45:22 +0530

    rev

--- src/practiceDigital/stringIteration.java
+++ src/practiceDigital/stringIteration.java
@@ -1,10 +1,29 @@
 package practiceDigital;
 
+import java.util.*;
+
 public class stringIteration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner a = new Scanner(System.in);
+		String str1 = a.nextLine();
+		String name = (str1.charAt(0)+". ");
+		
+		for (int i = 0; i <str1.length(); i++) {
+			if (str1.charAt(i) == ' ' && str1.charAt(i + 1) != ' ' && i + 1 < str1.length()) {
+				name = (name + str1.charAt(i + 1) + ". ");
+				break;
+			}
 
+		}
+		for(int i =str1.length()-1;i>=0;i--) {
+			if (str1.charAt(i)==' ') {
+				name = (name+str1.substring(i+1));
+				break;
+			}
+			
+		}
+		System.out.println(name);
 	}
 
 }
